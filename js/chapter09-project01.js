7b476007a67cb1be918b659755f695ac581ebf1c
Nilkumar Patel <=>
2021-01-25 10:52:34 -0500

    Group 8 Lab 2 Chapter 9 - Step 2

--- js/chapter09-project01.js
+++ js/chapter09-project01.js
@@ -1,2 +1,12 @@
 
 /* add code here  */
+let inputs = document.getElementsByClassName('hilightable');
+
+for (let i=0; i<inputs.length; i++) {
+    inputs[i].addEventListener('focus', () => {
+        inputs[i].classList.toggle('highlight')
+    });
+    inputs[i].addEventListener('blur', () => {
+        inputs[i].classList.toggle('highlight')
+    });
+}
